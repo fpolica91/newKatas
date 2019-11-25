b68e3e1becf3a919767e07c387fa28b85201f2bc
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-11-24 23:14:56 -0500

    dir reduce with reduce

--- directionReduce.js
+++ directionReduce.js
@@ -13,3 +13,18 @@ const dirReduc = (arr) => {
     }
     return arr
 }
+
+
+// using reduce 
+function dirReduc(plan) {
+    var opposite = {
+        'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'
+    };
+    return plan.reduce(function (dirs, dir) {
+        if (dirs[dirs.length - 1] === opposite[dir])
+            dirs.pop();
+        else
+            dirs.push(dir);
+        return dirs;
+    }, []);
+}
