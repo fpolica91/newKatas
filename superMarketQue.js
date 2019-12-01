a6ec811691222b4fd6882af829717b665fb93887
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-11-30 20:08:43 -0500

    quete

---
+++ superMarketQue.js
@@ -0,0 +1,14 @@
+const queueTime = (customers, registers) => {
+    resultsArr = []
+    if (registers === 0) {
+        return 0
+    } else {
+        let reg = queueTime(0, registers - 1)
+        resultsArr.push(0)
+        for (let i = 0; i < customers.length; i++) {
+            resultsArr[0] += customers[i]
+            resultsArr.sort()
+        }
+    }
+    return resultsArr[resultsArr.length - 1]
+}
