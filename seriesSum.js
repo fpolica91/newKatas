f63d3fe7e70c22995831d8f61d774ece4d43ff0e
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-11-30 21:07:21 -0500

    no for loop

---
+++ seriesSum.js
@@ -0,0 +1,18 @@
+// SERIES SUM USING RECURSION NO FOR LOOP
+
+function seriesSum(n) {
+    console.log(`N currently is ${n}`)
+    num = 1
+    top = 1
+    base = 1
+    if (n <= 0) {
+        return "0.00"
+    } else if (n == 1) {
+        return "1.00"
+    } else {
+        seriesSum(n - 1)
+        num += 3
+    }
+    return (base += (top / num)).toFixed(2)
+
+}
